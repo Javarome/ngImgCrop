ba33230e5eda43b9bd0f4c5554f0e16adaf8b252
Jerome Beau <jerome.beau@famicity.com>
2016-06-22 12:23:09 +0200

    Redraw on image-ready

--- compile/unminified/ng-img-crop.js
+++ compile/unminified/ng-img-crop.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2016 Alex Kaul
  * License: MIT
  *
- * Generated at Wednesday, June 22nd, 2016, 12:21:22 PM
+ * Generated at Wednesday, June 22nd, 2016, 12:23:00 PM
  */
 (function() {
 'use strict';
@@ -1374,7 +1374,7 @@ crop.service('cropEXIF', ['$log', function ($log) {
 }]);
 
 
-crop.factory('cropHost', ['$document', 'cropAreaCircle', 'cropAreaSquare', 'cropEXIF', '$log', '$timeout', function($document, CropAreaCircle, CropAreaSquare, cropEXIF, $log, $timeout) {
+crop.factory('cropHost', ['$document', 'cropAreaCircle', 'cropAreaSquare', 'cropEXIF', '$log', '$timeout', function($document, CropAreaCircle, CropAreaSquare, cropEXIF, $log) {
   /* STATIC FUNCTIONS */
 
   // Get Element's Offset
@@ -1465,8 +1465,8 @@ crop.factory('cropHost', ['$document', 'cropAreaCircle', 'cropAreaSquare', 'crop
           canvasDims[1] = minCanvasDims[1];
           canvasDims[0] = canvasDims[1] * imageRatio;
         }
-        const w = Math.floor(canvasDims[0]);
-        const h = Math.floor(canvasDims[1]);
+        var w = Math.floor(canvasDims[0]);
+        var h = Math.floor(canvasDims[1]);
         canvasDims[0] = w;
         canvasDims[1] = h;
         console.log('canvas reset =' + w + 'x' + h);
